b52d9df617e98f546199c15f1d98b4a088865957
FrancoB26 <161531648+FrancoB26@users.noreply.github.com>
2024-03-06 11:24:46 +0000

    practica_5

--- entrega/Guia 1/Ejercicio 12.py
+++ entrega/Guia 1/Ejercicio 12.py
@@ -1,5 +1,5 @@
 cantinvert=float(input("Ingrese la cantidad a invertir: "))
-intanual=float(input("Ingrese el interés anual (en porcentaje): "))
+intanual=float(input("Ingrese el interés anual: "))
 años=int(input("Ingrese el número de años: "))
 tasainteres=intanual/100
 capitalobtenido=cantinvert*(1+tasainteres)**años
