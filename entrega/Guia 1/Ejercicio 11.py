ecfac787254b469527c93894ee7bbb2735515841
FrancoB26 <161531648+FrancoB26@users.noreply.github.com>
2024-03-06 11:23:44 +0000

    practica_4

--- entrega/Guia 1/Ejercicio 11.py
+++ entrega/Guia 1/Ejercicio 11.py
@@ -1,4 +1,4 @@
-hstrabajadas = float(input("Ingrese el número de horas trabajadas: "))
-cthora = float(input("Ingrese el costo por hora: "))
-sueldo = hstrabajadas*cthora
+hstrabajadas=float(input("Ingrese el número de horas trabajadas: "))
+cthora=float(input("Ingrese el costo por hora: "))
+sueldo=hstrabajadas*cthora
 print("El sueldo es:", sueldo)
--- entrega/Guia 1/Ejercicio 8.py
+++ entrega/Guia 1/Ejercicio 8.py
@@ -1,11 +1,11 @@
-import math
-A=float(input("Ingrese uno de los lados o base del rectangulo"))
-B=float(input("Ingrese el otro lado o altura del rectangulo"))
-perimetro=A+B
-area=A*B
+pi=3.14
+a=float(input("Ingrese uno de los lados o base del rectangulo"))
+b=float(input("Ingrese el otro lado o altura del rectangulo"))
+perimetro=a+b
+area=a*b
 print(f"El area del rectangulo es {area}", f"su perimetro es{perimetro}")
 
-C=float(input("Ingrese el radio de la circunferencia"))
-perimetro2= 2*C*math.pi
-area2=math.pi*C**2
+c=float(input("Ingrese el radio de la circunferencia"))
+perimetro2= 2*c*pi
+area2=pi*c**2
 print(f"El area de la circunferencia es {area2}", f"su perimetro es{perimetro2}")
