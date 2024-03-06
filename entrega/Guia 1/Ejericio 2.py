96bd53863444b62f49477d856ca87962e3fb44ca
FrancoB26 <161531648+FrancoB26@users.noreply.github.com>
2024-03-06 09:47:25 +0000

    practica_632024

---
+++ entrega/Guia 1/Ejericio 2.py
@@ -0,0 +1,19 @@
+
+a=int(input("ingrese un numero:"))
+b=int(input("ingrese otro numero:"))
+c=int(a+b)
+d=int(a-b)
+e=a*b
+f=a/b
+g=a**b
+h=a//b
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+print(h)
+
+x=int(input("ingrese un numero:"))
+i=x**2+6*x+9
+print(i)
