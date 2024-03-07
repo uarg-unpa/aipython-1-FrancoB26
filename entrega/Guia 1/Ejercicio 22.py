d1e02bd30b2e26c03f2555b7c37e5964ee0d58dc
FrancoB26 <161531648+FrancoB26@users.noreply.github.com>
2024-03-07 13:45:32 +0000

    practica_6

--- entrega/Guia 1/Ejercicio 22.py
+++ entrega/Guia 1/Ejercicio 22.py
@@ -1,4 +1,2 @@
-#‘’El razonamiento matemático puede
-#considerarse más bien esquemáticamente como el ejercicio de una combinación de
-#dos instalaciones, que podemos llamar la intuición y el ingenio”
-
+frase="El razonamiento matemático puede considerarse más bien esquemáticamente como el ejercicio de una combinación de dos instalaciones, que podemos llamar la intuición y el ingenio"
+print(frase[0:15])
