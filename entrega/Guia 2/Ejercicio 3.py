169a1371a085be5fad0d96a3e2767e801d876897
FrancoB26 <161531648+FrancoB26@users.noreply.github.com>
2024-03-07 18:57:11 +0000

    practica_8

---
+++ entrega/Guia 2/Ejercicio 3.py
@@ -0,0 +1,6 @@
+a=input("Ingrese la contraseña: " )
+b="frAnco"
+if a==b :
+    print("La contraseña es correcta")
+else :
+    print("la contraseña es incorrecta")
---
+++ entrega/Guia 2/Ejercicio 5.py
@@ -0,0 +1,5 @@
+a=int(input("Ingrese un numero: "))
+if (a%2)==0:
+    print("El numero es par")
+else:
+    print("el numero es impar")
